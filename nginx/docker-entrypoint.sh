37f40dfc0b4ea776d0827f9ab567e3cfdd591dc1
Max Leske <maxleske@gmail.com>
2022-04-01 18:23:45 +0200

    Restricted find operation to the exact plugins path
    The expressions used with sed require extended regex support

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -18,12 +18,12 @@ done
 # Note: we are careful here to not assume the existance of the "plugins"
 # directory. It is being introduced with version 4 of CRS.
 for suffix in "config" "before" "after"; do
-    if [ -n "$(find /opt/owasp-crs -path "*plugins/*-${suffix}.conf")" ]; then
+    if [ -n "$(find /opt/owasp-crs -path "/opt/owasp-crs/plugins/*-${suffix}.conf")" ]; then
         # enable if there are config files
-        sed -i "s/#\s*\(.+-${suffix}.conf\)/\1/" /etc/modsecurity.d/setup.conf
+        sed -i -E "s/^#\s*(.+-${suffix}\.conf)/\1/" /etc/modsecurity.d/setup.conf
     else
         # disable if there are no config files
-        sed -i "s/\([^#]+-${suffix}.conf\)/# \1/" /etc/modsecurity.d/setup.conf
+        sed -i -E "s/^([^#]+-${suffix}\.conf)/# \1/" /etc/modsecurity.d/setup.conf
     fi
 done
 exec "$@"
