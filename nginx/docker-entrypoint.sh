5eae48ec287b90386e817d5c8898ea0d1a57de06
Max Leske <maxleske@gmail.com>
2022-04-06 16:00:47 +0200

    Added includes needed for plugins (#66)
    * Added includes needed for plugins
    
    * added work around for ModSecurity not supporting optional includes
    
    includes for plugins are now also disabled if no appropriate rules could be found
    
    this is necessary because plugins are likely to be added and removed, depending on the test

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -14,4 +14,16 @@ done
 
 . /opt/modsecurity/activate-rules.sh
 
+# Work around ModSecurity not supporting optional includes on NGiNX
+# Note: we are careful here to not assume the existance of the "plugins"
+# directory. It is being introduced with version 4 of CRS.
+for suffix in "config" "before" "after"; do
+    if [ -n "$(find /opt/owasp-crs -path "/opt/owasp-crs/plugins/*-${suffix}.conf")" ]; then
+        # enable if there are config files
+        sed -i -E "s/^#\s*(.+-${suffix}\.conf)/\1/" /etc/modsecurity.d/setup.conf
+    else
+        # disable if there are no config files
+        sed -i -E "s/^([^#]+-${suffix}\.conf)/# \1/" /etc/modsecurity.d/setup.conf
+    fi
+done
 exec "$@"
