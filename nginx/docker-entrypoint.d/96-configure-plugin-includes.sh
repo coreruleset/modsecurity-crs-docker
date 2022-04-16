b1a0d35ab4601400d993f54f6b764330b56c53fc
Max Leske <maxleske@gmail.com>
2022-04-16 16:33:34 +0200

    Adapted Dockerfiles and resources to use base image setup

--- nginx/docker-entrypoint.d/96-configure-plugin-includes.sh
+++ nginx/docker-entrypoint.d/96-configure-plugin-includes.sh
@@ -1,19 +1,3 @@
-#!/bin/sh -e
-
-export DNS_SERVER=${DNS_SERVER:-$(grep -i '^nameserver' /etc/resolv.conf|head -n1|cut -d ' ' -f2)}
-
-ENV_VARIABLES=$(awk 'BEGIN{for(v in ENVIRON) print "$"v}')
-
-FILES="/etc/nginx/nginx.conf /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/logging.conf /etc/nginx/includes/location_common.conf /etc/modsecurity.d/modsecurity-override.conf"
-
-for FILE in $FILES; do
-    if [ -f "$FILE" ]; then
-        envsubst "$ENV_VARIABLES" <"$FILE" | sponge "$FILE"
-    fi
-done
-
-. /opt/modsecurity/activate-rules.sh
-
 # Work around ModSecurity not supporting optional includes on NGiNX
 # Note: we are careful here to not assume the existance of the "plugins"
 # directory. It is being introduced with version 4 of CRS.
@@ -26,4 +10,3 @@ for suffix in "config" "before" "after"; do
         sed -i -E "s/^([^#]+-${suffix}\.conf)/# \1/" /etc/modsecurity.d/setup.conf
     fi
 done
-exec "$@"
