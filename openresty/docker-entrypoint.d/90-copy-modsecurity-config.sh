ac120e0fe25d97d0eb61d4e5fe66629767bb3d0c
Taavi Ansper <taavi.ansper@cyber.ee>
2023-02-16 11:39:35 +0200

    Remove more stuff

--- openresty/docker-entrypoint.d/90-copy-modsecurity-config.sh
+++ openresty/docker-entrypoint.d/90-copy-modsecurity-config.sh
@@ -9,6 +9,6 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 touch /etc/modsecurity.d/modsecurity-override.conf 2>/dev/null || { echo >&2 "$ME: error: can not modify /etc/modsecurity.d/modsecurity-override.conf (read-only file system?)"; exit 1; }
 
-cp /etc/nginx/modsecurity.d/*.conf /etc/modsecurity.d 2>/dev/null || { echo >&2 "$ME: error: cannot copy config files to /etc/modsecurity.d"; exit 2; }
+cp /usr/local/openresty/nginx/modsecurity.d/*.conf /etc/modsecurity.d 2>/dev/null || { echo >&2 "$ME: error: cannot copy config files to /etc/modsecurity.d"; exit 2; }
 
 exit 0
